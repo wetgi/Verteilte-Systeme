9ec17791d12ab5ed8f2ad31904c3c21cbdf09564
Gilles Wetzel <gilles@wetzel.lu>
2016-12-09 20:06:25 +0100

    Add Discovery

--- EShopSOA/microservice-eshop-category/src/main/java/de/hska/MicroserviceEshopCategoryApplication.java
+++ EShopSOA/microservice-eshop-category/src/main/java/de/hska/MicroserviceEshopCategoryApplication.java
@@ -2,9 +2,10 @@ package de.hska;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicroserviceEshopCategoryApplication {
 
 	public static void main(String[] args) {
