76ae65b08f29c0cbfac718c5f0f687b096c96af3
Gilles Wetzel <gilles@wetzel.lu>
2016-12-09 21:08:14 +0100

    Init Product-Category Service
    Product-Category Service (Composite)

---
+++ EShopSOA/composite/product-category-service/src/main/java/de/hska/ProductCategoryServiceApplication.java
@@ -0,0 +1,12 @@
+package de.hska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCategoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCategoryServiceApplication.class, args);
+	}
+}
