05ab49a108267b53ad7ba7c2777ebf802b0bea10
Gilles Wetzel <gilles@wetzel.lu>
2016-12-09 19:59:21 +0100

    Add Category Microservice

---
+++ EShopSOA/microservice-eshop-category/src/main/java/de/hska/MicroserviceEshopCategoryApplication.java
@@ -0,0 +1,13 @@
+package de.hska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MicroserviceEshopCategoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceEshopCategoryApplication.class, args);
+	}
+}
