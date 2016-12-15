06c20232730ba917ebdc6ebf559faff1eb3c56a1
Florian Dunz <dufl1012@hs-karlsruhe.de>
2016-12-15 15:22:47 +0100

    disabled create and drop of db table
    added product creation

--- EShopSOA/core/products-service/src/main/java/de/hska/api/ProductsApiController.java
+++ EShopSOA/core/products-service/src/main/java/de/hska/api/ProductsApiController.java
@@ -16,38 +16,42 @@ import de.hska.model.Product;
 import de.hska.repository.ProductRepository;
 import io.swagger.annotations.ApiParam;
 
-
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.SpringCodegen", date = "2016-12-08T10:50:15.291Z")
 
 @Controller
 public class ProductsApiController implements ProductsApi {
-	
+
 	@Autowired
-    private ProductRepository productRepository;
-	
-    public ResponseEntity<List<Product>> productsGet(@ApiParam(value = "Is contained in product name?") @RequestParam(value = "searchString", required = false) String searchString,
-        @ApiParam(value = "Does product cost at least x?") @RequestParam(value = "minPrice", required = false) Double minPrice,
-        @ApiParam(value = "Does product cost at max x?") @RequestParam(value = "maxPrice", required = false) Double maxPrice) {
-    	List<Product> list = new ArrayList<>();
-    	productRepository.findAll().forEach(list::add);
-        return new ResponseEntity<List<Product>>(list, HttpStatus.OK);
-    }
+	private ProductRepository productRepository;
 
-    public ResponseEntity<Product> productsPost(@ApiParam(value = "Information about the new product." ,required=true ) @RequestBody Product newProduct,
-        @ApiParam(value = "The requesting user" ,required=true ) @RequestHeader(value="userId", required=true) Integer userId) {
-        // do some magic!
-        return new ResponseEntity<Product>(HttpStatus.OK);
-    }
+	public ResponseEntity<List<Product>> productsGet(
+			@ApiParam(value = "Is contained in product name?") @RequestParam(value = "searchString", required = false) String searchString,
+			@ApiParam(value = "Does product cost at least x?") @RequestParam(value = "minPrice", required = false) Double minPrice,
+			@ApiParam(value = "Does product cost at max x?") @RequestParam(value = "maxPrice", required = false) Double maxPrice) {
+		List<Product> list = new ArrayList<>();
+		productRepository.findAll().forEach(list::add);
+		return new ResponseEntity<List<Product>>(list, HttpStatus.OK);
+	}
 
-    public ResponseEntity<Void> productsProductIdDelete(@ApiParam(value = "Deletes a product",required=true ) @PathVariable("productId") Integer productId,
-        @ApiParam(value = "The requesting user" ,required=true ) @RequestHeader(value="userId", required=true) Integer userId) {
-        // do some magic!
-        return new ResponseEntity<Void>(HttpStatus.OK);
-    }
+	public ResponseEntity<Product> productsPost(
+			@ApiParam(value = "Information about the new product.", required = true) @RequestBody Product newProduct,
+			@ApiParam(value = "The requesting user", required = true) @RequestHeader(value = "userId", required = true) Integer userId) {
+		// !TODO check if user is eligible
+		productRepository.save(newProduct);
+		return new ResponseEntity<Product>(HttpStatus.OK);
+	}
 
-    public ResponseEntity<Product> productsProductIdGet(@ApiParam(value = "Get details for product",required=true ) @PathVariable("productId") Integer productId) {
-        // do some magic!
-        return new ResponseEntity<Product>(HttpStatus.OK);
-    }
+	public ResponseEntity<Void> productsProductIdDelete(
+			@ApiParam(value = "Deletes a product", required = true) @PathVariable("productId") Integer productId,
+			@ApiParam(value = "The requesting user", required = true) @RequestHeader(value = "userId", required = true) Integer userId) {
+		// do some magic!
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+
+	public ResponseEntity<Product> productsProductIdGet(
+			@ApiParam(value = "Get details for product", required = true) @PathVariable("productId") Integer productId) {
+		// do some magic!
+		return new ResponseEntity<Product>(HttpStatus.OK);
+	}
 
 }
