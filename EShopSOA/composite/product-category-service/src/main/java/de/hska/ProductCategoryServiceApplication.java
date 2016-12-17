c7499697d78d94fb40097efe35e632fe2f698b48
Florian Dunz <dufl1012@hs-karlsruhe.de>
2016-12-17 15:02:29 +0100

    enabled turbine for hystrix dashboard

--- EShopSOA/composite/product-category-service/src/main/java/de/hska/ProductCategoryServiceApplication.java
+++ EShopSOA/composite/product-category-service/src/main/java/de/hska/ProductCategoryServiceApplication.java
@@ -4,13 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
-@EnableHystrixDashboard
+@EnableHystrix
 @RibbonClient("product-category-service")
 public class ProductCategoryServiceApplication {
 
--- EShopSOA/support/edge-server/src/main/java/de/hska/MicroserviceEshopZuulApplication.java
+++ EShopSOA/support/edge-server/src/main/java/de/hska/MicroserviceEshopZuulApplication.java
@@ -2,6 +2,9 @@ package de.hska;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
@@ -9,6 +12,9 @@ import de.hska.filters.pre.PreFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableHystrixDashboard
+@EnableHystrix
+@EnableTurbine
 public class MicroserviceEshopZuulApplication {
 
 	public static void main(String[] args) {
@@ -16,7 +22,7 @@ public class MicroserviceEshopZuulApplication {
 	}
 
 	@Bean
-	public PreFilter preFilder() {
+	public PreFilter preFilter() {
 		return new PreFilter();
 	}
 }
