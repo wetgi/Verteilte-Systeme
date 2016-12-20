97a1f76e93c813983907c72c3164709ab3707975
Florian Dunz <dufl1012@hs-karlsruhe.de>
2016-12-20 19:41:48 +0100

    further preparations for request filtering

--- EShopSOA/support/edge-server/src/main/java/de/hska/filters/pre/PreFilter.java
+++ EShopSOA/support/edge-server/src/main/java/de/hska/filters/pre/PreFilter.java
@@ -2,8 +2,10 @@ package de.hska.filters.pre;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
@@ -35,7 +37,22 @@ public class PreFilter extends ZuulFilter {
 
 		log.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
 
+		if (request.getMethod().equals(RequestMethod.DELETE)) {
+			String header = request.getHeader("userid");
+			if (header != null && !header.isEmpty() && NumberUtils.isNumber(header)) {
+				int userId = Integer.parseInt(header);
+				boolean isAdmin = isAdmin(userId);
+				// !TODO cancel this request if user is no admin
+			}
+		}
+
 		return null;
+	}
+
+	private boolean isAdmin(int userId) {
+		// !TODO call to rest
+		// http://localhost:8081/user-api/users/isadmin/ + userId
+		return false;
 	}
 
 }
